ece648ca32389e6b96390072fd053543ac06a276
Johnny Lee <contactjohnlee@gmail.com>
2025-02-13 22:36:30 -0600

    rename

--- features/search/Search.tsx
+++ features/search/Search.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from "react";
-import SearchModule from "./SearchModule"; // client component
+import SearchField from "./SearchField"; // client component
 import SearchResultsServer from "./SearchResultsServer"; // server component
 
 export default function Search({ query }: { query?: string }) {
     return (
         <div>
-            <SearchModule />
+            <SearchField />
             <Suspense fallback={<p>Loading results...</p>}>
                 <SearchResultsServer query={query || ""} />
             </Suspense>
