b82c0489becd26a04933d5d490e1c040aebb23f8
Johnny Lee <contactjohnlee@gmail.com>
2025-02-17 18:01:16 -0600

    eslintrc fix

--- features/collection/Collection.tsx
+++ features/collection/Collection.tsx
@@ -16,17 +16,19 @@ export default async function Collection({ username }: { username: string }) {
             {collection.length > 0 ? (
                 <ul>
                     {collection.map((item) => (
-                        <li key={item.id}>
-                            <strong>{item.basic_information.title}</strong> ({item.basic_information.year})
-                            {item.basic_information.cover_image && (
-                                <Image
-                                    src={item.basic_information.cover_image}
-                                    alt={item.basic_information.title}
-                                    width={100}
-                                    height={100}
-                                />
-                            )}
-                        </li>
+                        item.basic_information ? (
+                            <li key={item.id}>
+                                <strong>{item.basic_information.title}</strong> ({item.basic_information.year})
+                                {item.basic_information.cover_image && (
+                                    <Image
+                                        src={item.basic_information.cover_image}
+                                        alt={item.basic_information.title?? "Album image"}
+                                        width={100}
+                                        height={100}
+                                    />
+                                )}
+                            </li>
+                        ) : null
                     ))}
                 </ul>
             ) : (
